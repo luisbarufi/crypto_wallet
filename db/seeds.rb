69db36d42ff25ac32bac6fa671ae0f04eaeb1826
luisbarufi <luisbarufi@gmail.com>
2020-07-23 20:49:25 -0300

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coin.create!(
+  description: "Bitcoin",
+  acronym: "BTC",
+  url_image: "https://cdn.xbinop.com/br/wp-content/uploads/sites/8/2017/12/bitcoin.png"
+)
+
+Coin.create!(
+  description: "Ethereum",
+  acronym: "ETH",
+  url_image: "https://foxbit.com.br/wp-content/uploads/2019/01/Ethereum-01.png"
+)
+
+Coin.create!(
+  description: "Dash",
+  acronym: "DASH",
+  url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/660.png"
+)
