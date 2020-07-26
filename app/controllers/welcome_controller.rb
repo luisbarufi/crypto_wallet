8e8b50ecb0a8f8df53741b94482412018c2484a2
luisbarufi <luisbarufi@gmail.com>
2020-07-26 18:29:32 -0300

    knowing cookies and session

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:course] = "Curso de Ruby on Rails [COOKIE]"
+    session[:course] = "Curso de Ruby on Rails [SESSION]"
     @name = params[:name]
     @course = params[:course]
   end
