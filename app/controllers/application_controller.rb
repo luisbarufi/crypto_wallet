d6e652467098ae9d3bbc4bd81ad95f38efdabe0a
luisbarufi <luisbarufi@gmail.com>
2020-07-27 01:04:36 -0300

    fixing I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    session[:locale] = params[:locale] if params[:locale].present?    
+    locale = session[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end  
 end
