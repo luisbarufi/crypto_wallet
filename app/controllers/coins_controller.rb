caca8e3a4a40b7df4f610082c711d70df204270a
luisbarufi <luisbarufi@gmail.com>
2020-08-02 17:24:54 -0300

    fixing I18n

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -59,7 +59,7 @@ class CoinsController < ApplicationController
   def destroy
     @coin.destroy
     respond_to do |format|
-      format.html { redirect_to coins_url, notice: "#{t('Coin_was_successfully_destroyed')}." }
+      format.html { redirect_to coins_url, notice: "#{t('coin_was_successfully_destroyed')}." }
       format.json { head :no_content }
     end
   end
